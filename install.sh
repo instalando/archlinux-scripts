3de1d6726e4ff7ba27a134133f38db7e9c237ba6
instalando <instalando@disroot.org>
2020-01-02 02:05:39 +0000

    .

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+pacman -Sy reflector &&
+reflector -c Brazil > /etc/pacman.d/mirrorlist &&
 pacstrap -i /mnt base base-devel &&
 arch-chroot /mnt &&
 echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen &&
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf &&
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf &&
 locale-gen &&
+pacman -Sy reflector &&
+reflector -c Brazil > /etc/pacman.d/mirrorlist &&
 pacman -S linux linux-headers sudo xorg xorg-server gdm gnome gnome-extra vlc engrampa p7zip unrar unzip chromium firefox telegram-desktop mcomix dbeaver gnome-tweaks virtualbox pamac bauh gvfs gvfs-mtp leafpad okular efibootmgr grub reflector uget libreoffice networkmanager network-manager-applet &&
 systemctl enable NetworkManager &&
 systemctl enable gdm
