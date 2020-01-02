731b9553b763c318506bd9b00ca776174edabe0f
instalando <instalando@disroot.org>
2020-01-02 02:11:21 +0000

    .

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 pacman -Sy reflector &&
 reflector -c Brazil > /etc/pacman.d/mirrorlist &&
 pacstrap -i /mnt base base-devel &&
-cp scripts /mnt/scripts
+cp scripts /mnt/scripts &&
 arch-chroot /mnt
