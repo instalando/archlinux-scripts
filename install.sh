9d98fe4317acaa7348ef4e7ef1be4ee4c272a7f8
instalando <instalando@disroot.org>
2020-01-02 01:53:29 +0000

    .

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacman -S linux linux-headers sudo xorg xorg-server gdm gnome gnome-extra vlc engrampa p7zip unrar unzip chromium firefox telegram-desktop mcomix dbeaver gnome-tweaks virtualbox pamac bauh gvfs gvfs-mtp leafpad okular efibootmgr grub reflector uget libreoffice
+pacman -S linux linux-headers sudo xorg xorg-server gdm gnome gnome-extra vlc engrampa p7zip unrar unzip chromium firefox telegram-desktop mcomix dbeaver gnome-tweaks virtualbox pamac bauh gvfs gvfs-mtp leafpad okular efibootmgr grub reflector uget libreoffice networkmanager network-manager-applet
