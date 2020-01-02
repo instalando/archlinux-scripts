f5d62d4d5bde1ca455ebb0b7d5b0bf839c324230
instalando <instalando@disroot.org>
2020-01-02 02:03:37 +0000

    .

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-pacman -S linux linux-headers sudo xorg xorg-server gdm gnome gnome-extra vlc engrampa p7zip unrar unzip chromium firefox telegram-desktop mcomix dbeaver gnome-tweaks virtualbox pamac bauh gvfs gvfs-mtp leafpad okular efibootmgr grub reflector uget libreoffice networkmanager network-manager-applet
+pacstrap -i /mnt base base-devel &&
+arch-chroot /mnt &&
+echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen &&
+echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf &&
+echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf &&
+locale-gen &&
+pacman -S linux linux-headers sudo xorg xorg-server gdm gnome gnome-extra vlc engrampa p7zip unrar unzip chromium firefox telegram-desktop mcomix dbeaver gnome-tweaks virtualbox pamac bauh gvfs gvfs-mtp leafpad okular efibootmgr grub reflector uget libreoffice networkmanager network-manager-applet &&
+systemctl enable NetworkManager &&
+systemctl enable gdm
+
