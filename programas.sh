e8ccacb8d2e68c74c0ea479738e6062a9e9e6adc
instalando <instalando@disroot.org>
2020-01-02 02:33:54 +0000

    .

--- programas.sh
+++ programas.sh
@@ -2,7 +2,7 @@
 
 pacman -Sy reflector &&
 reflector -c Brazil > /etc/pacman.d/mirrorlist &&
-pacman -S linux linux-headers sudo xorg xorg-server gdm gnome gnome-extra vlc engrampa p7zip unrar unzip chromium firefox telegram-desktop mcomix dbeaver gnome-tweaks virtualbox pamac bauh gvfs gvfs-mtp leafpad okular efibootmgr grub reflector uget libreoffice networkmanager network-manager-applet &&
+pacman -S linux linux-headers sudo xorg xorg-server gdm gnome gnome-extra vlc engrampa p7zip unrar unzip chromium firefox telegram-desktop mcomix dbeaver gnome-tweaks virtualbox gvfs gvfs-mtp leafpad okular efibootmgr grub reflector uget libreoffice networkmanager network-manager-applet &&
 systemctl enable NetworkManager &&
 systemctl enable gdm
 
